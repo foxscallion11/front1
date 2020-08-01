4c8b91abe89a388ed2bb227ecd1e0ac6a1b3b970
foxscallion <1969223475@qq.com>
2020-08-01 23:19:28 +0800

    first1

---
+++ api/banner.js
@@ -0,0 +1,9 @@
+import request from '@/utils/request'
+export default {
+  getBannerList() {
+    return request({
+      url: `/educms/bannerfront/getAllBanner`,
+      method: 'get'
+    })
+  },
+}
